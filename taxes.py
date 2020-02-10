b6189e8c71b9720cf001b8b94387fd8a31e8ce50
titov-vv <55182132+titov-vv@users.noreply.github.com>
2020-02-10 19:08:41 +0300

    First draft of Taxes export to Excel file, deals added

--- taxes.py
+++ taxes.py
@@ -9,20 +9,42 @@ class TaxesRus:
         year_begin = int(time.mktime(datetime.datetime.strptime(f"{year}", "%Y").timetuple()))
         year_end = int(time.mktime(datetime.datetime.strptime(f"{year+1}", "%Y").timetuple()))
 
-        dividends_query = QSqlQuery(self.db)
-        dividends_query.prepare("SELECT d.timestamp, a.name, a.full_name, d.sum, d.sum_tax FROM dividends AS d "
-                                "LEFT JOIN assets AS a ON a.id = d.asset_id "
-                                "WHERE d.timestamp>=:start AND d.timestamp<:end AND d.account_id=:account_id")
-        dividends_query.bindValue(":start", year_begin)
-        dividends_query.bindValue(":end", year_end)
-        dividends_query.bindValue(":account_id", account_id)
-        dividends_query.exec_()
-
         wbk = xlwt.Workbook()
+
+        query = QSqlQuery(self.db)
+        query.prepare("SELECT d.timestamp, a.name, a.full_name, d.sum, d.sum_tax FROM dividends AS d "
+                                "LEFT JOIN assets AS a ON a.id = d.asset_id "
+                                "WHERE d.timestamp>=:begin AND d.timestamp<:end AND d.account_id=:account_id")
+        query.bindValue(":begin", year_begin)
+        query.bindValue(":end", year_end)
+        query.bindValue(":account_id", account_id)
+        query.exec_()
+
         sheet = wbk.add_sheet("Dividends", cell_overwrite_ok=True)
         row = 0
-        while dividends_query.next():
-            for column in range(dividends_query.record().count()):
-                sheet.write(row, column, dividends_query.value(column))
+        while query.next():
+            for column in range(query.record().count()):
+                sheet.write(row, column, query.value(column))
             row = row + 1
+
+        query.prepare("SELECT a.name, d.qty, o.timestamp, o.settlement, o.price, o.fee, "
+                      "c.timestamp, c.settlement, c.price, c.fee FROM deals AS d "
+                      "LEFT JOIN sequence AS os ON os.id=d.open_sid "
+                      "LEFT JOIN trades AS o ON os.operation_id=o.id "
+                      "LEFT JOIN sequence AS cs ON cs.id=d.close_sid "
+                      "LEFT JOIN trades AS c ON cs.operation_id=c.id "
+                      "LEFT JOIN assets AS a ON d.asset_id=a.id "
+                      "WHERE c.timestamp>=:begin AND c.timestamp<:end AND d.account_id=:account_id")
+        query.bindValue(":begin", year_begin)
+        query.bindValue(":end", year_end)
+        query.bindValue(":account_id", account_id)
+        query.exec_()
+
+        sheet = wbk.add_sheet("Deals", cell_overwrite_ok=True)
+        row = 0
+        while query.next():
+            for column in range(query.record().count()):
+                sheet.write(row, column, query.value(column))
+            row = row + 1
+
         wbk.save(taxes_file)
