6f8d22dc4a8f584541cdff871354afb58cc02b53
titov-vv <55182132+titov-vv@users.noreply.github.com>
2020-08-10 16:24:31 +0300

    Exception logger was added to put information about unhandled exceptions into log window.

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python
 
 import sys
+import logging
+import traceback
 from PySide2.QtWidgets import QApplication
 from PySide2 import QtCore
 from main_window import MainWindow
 
+def exception_logger(exctype, value, tb):
+    info = traceback.format_exception(exctype, value, tb)
+    logging.fatal(f"EXCEPTION: {info}")
+    sys._excepthook(exctype, value, tb)
+
 if __name__ == "__main__":
+    sys.excepthook = exception_logger
+
     app = QApplication([])
     # font = app.font()
     # font.setPixelSize(15)
