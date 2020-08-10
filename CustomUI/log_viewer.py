d0f0fe4ea4d2031cbe64f9ea40e0e731c5dddd77
titov-vv <55182132+titov-vv@users.noreply.github.com>
2020-08-10 19:00:01 +0300

    Status of log messages is now highlighted with different colors.

--- CustomUI/log_viewer.py
+++ CustomUI/log_viewer.py
@@ -1,4 +1,5 @@
 import logging
+from constants import *
 
 from PySide2 import QtWidgets
 from PySide2.QtWidgets import QPlainTextEdit
@@ -10,8 +11,9 @@ class LogViewer(QPlainTextEdit, logging.Handler):
         logging.Handler.__init__(self)
         self.app = QtWidgets.QApplication.instance()
         self.setReadOnly(True)
-        self.notification = None
-        self.last_level = 0
+        self.notification = None  # Here is QLabel element to display LOG update status
+        self.clear_color = None   # Variable to store initial "clear" background color
+        self.last_level = 0       # Max last log level
 
     def emit(self, record, **kwargs):
         # Store message in log window
@@ -21,18 +23,32 @@ class LogViewer(QPlainTextEdit, logging.Handler):
         # Raise flag if status bar is set
         if self.notification:
             if self.last_level < record.levelno:
-                self.notification.setText("Log: " + record.levelname[0])
+                palette = self.notification.palette()
+                if record.levelno <= logging.INFO:
+                    palette.setColor(self.notification.backgroundRole(), LIGHT_BLUE_COLOR)
+                elif record.levelno <= logging.WARNING:
+                    palette.setColor(self.notification.backgroundRole(), LIGHT_YELLOW_COLOR)
+                else:
+                    palette.setColor(self.notification.backgroundRole(), LIGHT_RED_COLOR)
+                self.notification.setPalette(palette)
+                self.notification.setText("LOG")
+                self.last_level = record.levelno
 
         self.app.processEvents()
 
     def setNotificationLabel(self, label):
         self.notification = label
-        self.notification.setFixedWidth(self.notification.fontMetrics().width("Log: X"))
+        self.notification.setFixedWidth(self.notification.fontMetrics().width("LOG"))
+        self.notification.setAutoFillBackground(True)
+        self.clear_color = self.notification.palette().color(self.notification.backgroundRole())
 
     def removeNotificationLabel(self):
-        self.notification.setText("")
+        self.cleanNotification()
         self.notification = None
 
     def cleanNotification(self):
         self.last_level = 0
+        palette = self.notification.palette()
+        palette.setColor(self.notification.backgroundRole(), self.clear_color)
+        self.notification.setPalette(palette)
         self.notification.setText("")
