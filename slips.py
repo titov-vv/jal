71169e0281a6580120dc2dc02c4e0b02c5275e37
titov-vv <55182132+titov-vv@users.noreply.github.com>
2020-08-28 13:57:27 +0300

    Draft code for QR recognition from webcamera changed from OpenCV usage to pure Qt code is it doesn't require extra dependency

--- slips.py
+++ slips.py
@@ -1,15 +1,15 @@
 import io
 import re
+import logging
 # below needed for QR recognition
 from pyzbar import pyzbar
 from PIL import Image
-# below are for camera recognition
-import cv2
-import imutils
-from imutils.video import WebcamVideoStream
 
-from PySide2.QtCore import QDateTime, QBuffer
+from PySide2.QtCore import QDateTime, QBuffer, QThread
 from PySide2.QtWidgets import QApplication, QDialog, QFileDialog
+# This QCamera staff ran good on Windows but didn't fly on Linux from the box until 'cheese' installation
+from PySide2.QtMultimedia import QCameraInfo, QCamera, QCameraImageCapture, QVideoFrame
+from PySide2.QtMultimediaWidgets import QCameraViewfinder
 from CustomUI.helpers import g_tr
 from UI.ui_slip_import_dlg import Ui_ImportSlipDlg
 
@@ -25,6 +25,10 @@ class ImportSlipDialog(QDialog, Ui_ImportSlipDlg):
         self.LoadQRfromFileBtn.clicked.connect(self.loadQR)
         self.GetQRfromClipboardBtn.clicked.connect(self.readClipboardQR)
         self.GetQRfromCameraBtn.clicked.connect(self.readCameraQR)
+
+        self.cam = None
+        self.capture = None
+        self.viewfinder = None
 
     def loadQR(self):
         qr_file, _filter = \
@@ -48,24 +52,58 @@ class ImportSlipDialog(QDialog, Ui_ImportSlipDlg):
             print(f"Got QR: {barcode.data.decode('utf-8')}")
             self.parseQRdata(barcode.data.decode('utf-8'))
 
-    def readCameraQR(self):
-        vs = WebcamVideoStream(src=0).start()
-        while True:
-            frame = vs.read()                         # Grab the frame from the stream
-            frame = imutils.resize(frame, width=512)  # Resize to acceptable size
-            cv2.imshow("Barcode Scanner", frame)  # Display frame for user
-            barcodes = pyzbar.decode(frame, symbols=[pyzbar.ZBarSymbol.QRCODE])
-            if barcodes:
-                barcode= barcodes[0]
-                print(f"Found QR: {barcode.data.decode('utf-8')}")
-                self.parseQRdata(barcode.data.decode('utf-8'))
-                break
+    def log_error(self):
+        logging.info("Cam error")
 
-            key = cv2.waitKey(1) & 0xFF      # Break from loop if Q is pressed
-            if key == ord("q"):
-                break
-        cv2.destroyAllWindows()
-        vs.stop()
+    def processCaptureImage(self, id, preview):
+        print(f"captured #{id}")
+
+    def processSavedImage(self, id, name):
+        print(f"saved #{id} at {name}")
+
+    def readyForCapture(self):
+        print("capture ready")
+        self.capture.capture()
+
+    def processBufferedImage(self, id, frame):
+        print("img ready")
+        img = frame.image()
+        buffer = QBuffer()
+        buffer.open(QBuffer.ReadWrite)
+        img.save(buffer, "BMP")
+        pil_im = Image.open(io.BytesIO(buffer.data()))
+        barcodes = pyzbar.decode(pil_im, symbols=[pyzbar.ZBarSymbol.QRCODE])
+        if barcodes:
+            print(f"Got QR: {barcodes[0].data.decode('utf-8')}")
+            self.parseQRdata(barcodes[0].data.decode('utf-8'))
+            self.cam.stop()
+        else:
+            QThread.sleep(1)
+            self.capture.capture()
+
+    def CaptureError(self, id, error, msg):
+        print(f"capture error: {msg}")
+
+    def readCameraQR(self):
+        camera_info = QCameraInfo.defaultCamera()
+        self.cam = QCamera(camera_info)
+        self.cam.errorOccurred.connect(self.log_error)
+        self.capture = QCameraImageCapture(self.cam)
+
+        self.viewfinder = QCameraViewfinder()
+        self.viewfinder.show()
+        self.cam.setViewfinder(self.viewfinder)
+
+        self.capture.imageCaptured.connect(self.processCaptureImage)
+        self.capture.imageSaved.connect(self.processSavedImage)
+        self.capture.imageAvailable.connect(self.processBufferedImage)
+        self.capture.readyForCaptureChanged.connect(self.readyForCapture)
+        self.capture.error.connect(self.CaptureError)
+
+        self.capture.setCaptureDestination(QCameraImageCapture.CaptureToBuffer)
+        self.capture.setBufferFormat(QVideoFrame.Format_RGB32)
+        self.cam.setCaptureMode(QCamera.CaptureStillImage)
+        self.cam.start()
 
     def parseQRdata(self, data):
         parts = re.match(self.QR_pattern, data)
