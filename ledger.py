titov-vv/jal

commit e5e1b5dcb23502cfa04da4fbbf92825afc932ec9

1. Menu changed to add separate Reports group 2. Quik HTML-report load fixed to include fee and ignore extra spaces 3. Deals match for FIFO corrected 4. Cleanup of 'deals' table fixed for ledger rebuild

ledger.py
@@ -190,38 +190,36 @@ class Ledger:
         deal_query.bindValue(":close_sid", seq_id)
         query = QSqlQuery(self.db)
         if (self.getAmount(timestamp, BOOK_ACCOUNT_ASSETS, account_id, asset_id) < 0):
-            query.prepare("SELECT o.timestamp AS open, c.timestamp AS close, abs(c.qty)-d.qty AS remainder FROM deals AS d "
+            query.prepare("SELECT d.open_sid AS open, abs(o.qty) - SUM(d.qty) AS remainder FROM deals AS d "
                           "LEFT JOIN sequence AS os ON os.type=3 AND os.id=d.open_sid "
                           "JOIN trades AS o ON o.id = os.operation_id "
-                          "LEFT JOIN sequence AS cs ON cs.type=3 AND cs.id=d.close_sid "
-                          "JOIN trades AS c ON c.id = cs.operation_id "
                           "WHERE d.account_id=:account_id AND d.asset_id=:asset_id "
-                          "ORDER BY d.close_sid DESC, d.open_sid DESC")
+                          "GROUP BY d.open_sid "
+                          "ORDER BY d.close_sid DESC, d.open_sid DESC LIMIT 1")
             query.bindValue(":asset_id", asset_id)
             query.bindValue(":account_id", account_id)
             query.bindValue(":timestamp", timestamp)
             assert query.exec_()
             if query.next():
-                reminder = query.value(2)  # value(2) = non-matched reminder of last Sell trade
-                close_timestamp = query.value(1)  # value(1) = timestamp of Sell trade
+                reminder = query.value(1)  # value(1) = non-matched reminder of last Sell trade
+                last_sid = query.value(0)  # value(0) = sid of Sell trade from the last deal
                 if reminder:  # we have some not fully matched Sell trade - start from it
                     query.prepare("SELECT s.id, -t.qty, t.price FROM trades AS t "
                                   "LEFT JOIN sequence AS s ON s.type = 3 AND s.operation_id=t.id "
                                   "WHERE t.qty < 0 AND t.asset_id = :asset_id AND t.account_id = :account_id "
-                                  "AND s.id < :sid AND t.timestamp >= :last_deal")
+                                  "AND s.id < :sid AND s.id >= :last_sid")
                 else:   # start from next Sell trade
                     query.prepare("SELECT s.id, -t.qty, t.price FROM trades AS t "
                                   "LEFT JOIN sequence AS s ON s.type = 3 AND s.operation_id=t.id "
                                   "WHERE t.qty < 0 AND t.asset_id = :asset_id AND t.account_id = :account_id "
-                                  "AND s.id < :sid AND t.timestamp > :last_deal")
-                query.bindValue(":last_deal", close_timestamp)
+                                  "AND s.id < :sid AND s.id >= :last_sid")
+                query.bindValue(":last_sid", last_sid)
             else:  # There were no deals -> Select all sells
                 reminder = 0
                 query.prepare("SELECT s.id, -t.qty, t.price FROM trades AS t "
                               "LEFT JOIN sequence AS s ON s.type = 3 AND s.operation_id=t.id "
                               "WHERE t.qty < 0 AND t.asset_id=:asset_id AND t.account_id=:account_id AND s.id<:sid")
             query.bindValue(":asset_id", asset_id)
-            query.bindValue(":account_id", account_id)
             query.bindValue(":sid", seq_id)
             assert query.exec_()
             while query.next():
@@ -265,32 +263,30 @@ class Ledger:
         deal_query.bindValue(":close_sid", seq_id)
         query = QSqlQuery(self.db)
         if (self.getAmount(timestamp, BOOK_ACCOUNT_ASSETS, account_id, asset_id) > 0):
-            query.prepare(
-                "SELECT o.timestamp AS open, c.timestamp AS close, abs(c.qty)-d.qty AS remainder FROM deals AS d "
-                "LEFT JOIN sequence AS os ON os.type=3 AND os.id=d.open_sid "
-                "JOIN trades AS o ON o.id = os.operation_id "
-                "LEFT JOIN sequence AS cs ON cs.type=3 AND cs.id=d.close_sid "
-                "JOIN trades AS c ON c.id = cs.operation_id "
-                "WHERE d.account_id=:account_id AND d.asset_id=:asset_id "
-                "ORDER BY d.close_sid DESC, d.open_sid DESC")
+            query.prepare("SELECT d.open_sid AS open, abs(o.qty) - SUM(d.qty) AS remainder FROM deals AS d "
+                          "LEFT JOIN sequence AS os ON os.type=3 AND os.id=d.open_sid "
+                          "JOIN trades AS o ON o.id = os.operation_id "
+                          "WHERE d.account_id=:account_id AND d.asset_id=:asset_id "
+                          "GROUP BY d.open_sid "
+                          "ORDER BY d.close_sid DESC, d.open_sid DESC LIMIT 1")
             query.bindValue(":asset_id", asset_id)
             query.bindValue(":account_id", account_id)
             query.bindValue(":timestamp", timestamp)
             assert query.exec_()
             if query.next():
-                reminder = query.value(2)  # value(2) = non-matched reminder of last Sell trade
-                close_timestamp = query.value(1)  # value(1) = timestamp of Buy trade
+                reminder = query.value(1)  # value(1) = non-matched reminder of last Sell trade
+                last_sid = query.value(0)  # value(0) = sid of Buy trade from last deal
                 if reminder:  # we have some not fully matched Buy trade - start from it
                     query.prepare("SELECT s.id, t.qty, t.price FROM trades AS t "
                                   "LEFT JOIN sequence AS s ON s.type = 3 AND s.operation_id=t.id "
                                   "WHERE t.qty > 0 AND t.asset_id = :asset_id AND t.account_id = :account_id "
-                                  "AND s.id < :sid AND t.timestamp >= :last_deal")
+                                  "AND s.id < :sid AND s.id >= :last_sid")
                 else:  # start from next Buy trade
                     query.prepare("SELECT s.id, t.qty, t.price FROM trades AS t "
                                   "LEFT JOIN sequence AS s ON s.type = 3 AND s.operation_id=t.id "
                                   "WHERE t.qty > 0 AND t.asset_id = :asset_id AND t.account_id = :account_id "
-                                  "AND s.id < :sid AND t.timestamp > :last_deal")
-                query.bindValue(":last_deal", close_timestamp)
+                                  "AND s.id < :sid AND s.id >= :last_sid")
+                query.bindValue(":last_sid", last_sid)
             else:  # There were no deals -> Select all purchases
                 reminder = 0
                 query.prepare("SELECT s.id, t.qty, t.price FROM trades AS t "
@@ -408,7 +404,7 @@ class Ledger:
             frontier = 0
 
         query.prepare("DELETE FROM deals WHERE close_sid > "
-                      "(SELECT coalesce(MAX(id), 0) FROM sequence WHERE timestamp >= :frontier)")
+                      "(SELECT coalesce(MIN(id), 0) FROM sequence WHERE timestamp >= :frontier)")
         query.bindValue(":frontier", frontier)
         assert query.exec_()
         query.prepare("DELETE FROM ledger WHERE timestamp >= :frontier")
@@ -473,8 +469,8 @@ class Ledger:
         print(">> Re-build ledger from: ", datetime.datetime.fromtimestamp(frontier).strftime('%d/%m/%Y %H:%M:%S'))
         start_time = datetime.datetime.now()
         print(">> Started @", start_time)
-        query.prepare("DELETE FROM deals WHERE close_sid > "
-                      "(SELECT coalesce(MAX(id), 0) FROM sequence WHERE timestamp >= :frontier)")
+        query.prepare("DELETE FROM deals WHERE close_sid >= "
+                      "(SELECT coalesce(MIN(id), 0) FROM sequence WHERE timestamp >= :frontier)")
         query.bindValue(":frontier", frontier)
         assert query.exec_()
         query.prepare("DELETE FROM ledger WHERE timestamp >= :frontier")

UI/main_window.ui
@@ -1304,8 +1304,9 @@
    </widget>
    <widget class="QMenu" name="menu_Reports">
     <property name="title">
-     <string>&amp;Taxes</string>
+     <string>&amp;Reports</string>
     </property>
+    <addaction name="MakeDealsReport"/>
     <addaction name="PrepareTaxForms"/>
    </widget>
    <addaction name="menuFile"/>
@@ -1387,7 +1388,12 @@
   </action>
   <action name="PrepareTaxForms">
    <property name="text">
-    <string>&amp;Data for taxes [RU]</string>
+    <string>Report for &amp;taxes [RU]</string>
+   </property>
+  </action>
+  <action name="MakeDealsReport">
+   <property name="text">
+    <string>&amp;Deals report</string>
    </property>
   </action>
  </widget>

UI/ui_main_window.py
@@ -58,6 +58,8 @@ class Ui_LedgerMainWindow(object):
         self.actionRestore.setObjectName(u"actionRestore")
         self.PrepareTaxForms = QAction(LedgerMainWindow)
         self.PrepareTaxForms.setObjectName(u"PrepareTaxForms")
+        self.MakeDealsReport = QAction(LedgerMainWindow)
+        self.MakeDealsReport.setObjectName(u"MakeDealsReport")
         self.centralwidget = QWidget(LedgerMainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.centralwidget.setMaximumSize(QSize(16777215, 16777215))
@@ -756,6 +758,7 @@ class Ui_LedgerMainWindow(object):
         self.menuPredefined_data.addAction(self.actionAccountTypes)
         self.menuLoad.addAction(self.action_Load_quotes)
         self.menuLoad.addAction(self.actionLoad_Statement)
+        self.menu_Reports.addAction(self.MakeDealsReport)
         self.menu_Reports.addAction(self.PrepareTaxForms)
 
         self.retranslateUi(LedgerMainWindow)
@@ -782,7 +785,8 @@ class Ui_LedgerMainWindow(object):
         self.actionCategories.setText(QCoreApplication.translate("LedgerMainWindow", u"&Categories", None))
         self.actionBackup.setText(QCoreApplication.translate("LedgerMainWindow", u"&Backup...", None))
         self.actionRestore.setText(QCoreApplication.translate("LedgerMainWindow", u"&Restore...", None))
-        self.PrepareTaxForms.setText(QCoreApplication.translate("LedgerMainWindow", u"&Data for taxes [RU]", None))
+        self.PrepareTaxForms.setText(QCoreApplication.translate("LedgerMainWindow", u"Report for &taxes [RU]", None))
+        self.MakeDealsReport.setText(QCoreApplication.translate("LedgerMainWindow", u"&Deals report", None))
         self.BalanceBox.setTitle(QCoreApplication.translate("LedgerMainWindow", u"Balances", None))
         self.BalanceDate.setDisplayFormat(QCoreApplication.translate("LedgerMainWindow", u"dd/MM/yyyy", None))
         self.CurrencyLbl.setText(QCoreApplication.translate("LedgerMainWindow", u"Sum Currency:", None))
@@ -863,6 +867,6 @@ class Ui_LedgerMainWindow(object):
         self.menu_DAta.setTitle(QCoreApplication.translate("LedgerMainWindow", u"&Data", None))
         self.menuPredefined_data.setTitle(QCoreApplication.translate("LedgerMainWindow", u"Predefined data", None))
         self.menuLoad.setTitle(QCoreApplication.translate("LedgerMainWindow", u"&Load", None))
-        self.menu_Reports.setTitle(QCoreApplication.translate("LedgerMainWindow", u"&Taxes", None))
+        self.menu_Reports.setTitle(QCoreApplication.translate("LedgerMainWindow", u"&Reports", None))
     # retranslateUi
 

main_window.py
@@ -339,6 +339,7 @@ class MainWindow(QMainWindow, Ui_LedgerMainWindow):
         self.actionAssets.triggered.connect(self.EditAssets)
         self.actionPeers.triggered.connect(self.EditPeers)
         self.actionCategories.triggered.connect(self.EditCategories)
+        self.MakeDealsReport.triggered.connect(self.ReportDeals)
         self.PrepareTaxForms.triggered.connect(self.ExportTaxForms)
         # INTERFACE ACTIONS
         self.MainTabs.currentChanged.connect(self.OnMainTabChange)
@@ -886,6 +887,10 @@ class MainWindow(QMainWindow, Ui_LedgerMainWindow):
             if filter == self.tr("Quik HTML-report (*.htm)"):
                 report_loader.loadQuikHtml(report_file)
             self.UpdateLedger()
+
+    @Slot()
+    def ReportDeals(self):
+        pass
 
     @Slot()
     def ExportTaxForms(self):

statements.py
@@ -21,6 +21,18 @@ QUIK_COUPON = 'НКД'
 QUIK_SETTLEMENT = 'Дата расчётов'
 QUIK_BUY = 'Купля'
 QUIK_SELL = 'Продажа'
+QUIK_FEE1 = 'Комиссия Брокера'
+QUIK_FEE2 = 'Комиссия за ИТС'
+QUIK_FEE3 = 'Комиссия за организацию торговли'
+QUIK_FEE4 = 'Клиринговая комиссия'
+
+def convert_sum(val):
+    val = val.replace(' ', '')
+    try:
+        res = float(val)
+    except:
+        res = 0
+    return res
 
 class StatementLoader:
     def __init__(self, db):
@@ -421,7 +433,9 @@ class StatementLoader:
         print(f"Withholding tax added: {note}")
 
     def loadQuikHtml(self, filename):
-        data = pandas.read_html(filename, encoding='cp1251')
+        data = pandas.read_html(filename, encoding='cp1251',
+                                converters = {QUIK_QTY: convert_sum, QUIK_AMOUNT: convert_sum,
+                                              QUIK_PRICE: convert_sum, QUIK_COUPON: convert_sum})
         report_info = data[0]
         deals_info = data[1]
         parts = re.match(CLIENT_PATTERN, report_info[0][2])
@@ -442,10 +456,10 @@ class StatementLoader:
             timestamp = int(datetime.strptime(row[QUIK_TIMESTAMP], "%d.%m.%Y %H:%M:%S").timestamp())
             settlement = int(datetime.strptime(row[QUIK_SETTLEMENT], "%d.%m.%Y").timestamp())
             number = row[QUIK_DEAL_NUMBER]
-            price = float(row[QUIK_PRICE])
-            amount = float(row[QUIK_AMOUNT])
+            price = row[QUIK_PRICE]
+            amount = row[QUIK_AMOUNT]
             lot_size = math.pow(10, round(math.log10(amount / (price*abs(qty)))))
             qty = qty * lot_size
-            fee = 0  # there is monthly fee in Uralsib
-            coupon = float(row[QUIK_COUPON])
+            fee = float(row[QUIK_FEE1]) + float(row[QUIK_FEE2]) + float(row[QUIK_FEE3]) + float(row[QUIK_FEE4])
+            coupon = row[QUIK_COUPON]
             self.createTrade(account_id, asset_id, timestamp, settlement, number, qty, price, fee, coupon)

commit 5e4d2e770784dc9cb95e603ff258a3208ba114d2

ledger.py: Found a bug in processBuy()/processSell() that was introduced by e5e1b5d and wrongly "corrected" in dc7594d. The bug itself had been introduced in deals match algorithm by occasional '>' replacement with '>=' (due to wrong query copy-paste). Two branches of 'if-else' became identical as result of this mistake. And then one branch was totally removed by code optimization in subsequent commits. This condition branch is restored by this commit. Also change in dc7594d was incorrect - it is reverted.

ledger.py
@@ -273,7 +273,7 @@ class Ledger:
         sell_value = 0
         if self.getAmount(BookAccount.Assets, asset_id) < 0:
             query = executeSQL(self.db,
-                               "SELECT d.close_sid AS close, ABS(o.qty) - SUM(d.qty) AS remainder FROM deals AS d "
+                               "SELECT d.open_sid AS open, ABS(o.qty) - SUM(d.qty) AS remainder FROM deals AS d "
                                "LEFT JOIN sequence AS os ON os.type=3 AND os.id=d.open_sid "
                                "JOIN trades AS o ON o.id = os.operation_id "
                                "WHERE d.account_id=:account_id AND d.asset_id=:asset_id "
@@ -282,13 +282,22 @@ class Ledger:
                                [(":account_id", account_id), (":asset_id", asset_id)])
             if query.next():  # sid of Sell trade from the last deal and non-matched reminder of last Sell trade
                 last_sid, reminder = readSQLrecord(query)
-                query = executeSQL(self.db,
-                                   "SELECT s.id, -t.qty, t.price FROM trades AS t "
-                                   "LEFT JOIN sequence AS s ON s.type = 3 AND s.operation_id=t.id "
-                                   "WHERE t.qty < 0 AND t.asset_id = :asset_id AND t.account_id = :account_id "
-                                   "AND s.id < :sid AND s.id >= :last_sid",
-                                   [(":account_id", account_id), (":asset_id", asset_id),
-                                    (":sid", seq_id), (":last_sid", last_sid)])
+                if reminder < 0:   # Last Buy didn't fully match Sell Trade and we need to start from this partial Sell
+                    query = executeSQL(self.db,
+                                       "SELECT s.id, -t.qty, t.price FROM trades AS t "
+                                       "LEFT JOIN sequence AS s ON s.type = 3 AND s.operation_id=t.id "
+                                       "WHERE t.qty < 0 AND t.asset_id = :asset_id AND t.account_id = :account_id "
+                                       "AND s.id < :sid AND s.id >= :last_sid",
+                                       [(":account_id", account_id), (":asset_id", asset_id),
+                                        (":sid", seq_id), (":last_sid", last_sid)])
+                else:   # Sell trade was fully matched by Buy in the last deal - we need to take next Sell Trade
+                    query = executeSQL(self.db,
+                                       "SELECT s.id, -t.qty, t.price FROM trades AS t "
+                                       "LEFT JOIN sequence AS s ON s.type = 3 AND s.operation_id=t.id "
+                                       "WHERE t.qty < 0 AND t.asset_id = :asset_id AND t.account_id = :account_id "
+                                       "AND s.id < :sid AND s.id > :last_sid",
+                                       [(":account_id", account_id), (":asset_id", asset_id),
+                                        (":sid", seq_id), (":last_sid", last_sid)])
             else:  # There were no deals -> Select all sells
                 reminder = 0
                 query = executeSQL(self.db,
@@ -298,7 +307,7 @@ class Ledger:
                                    [(":account_id", account_id), (":asset_id", asset_id), (":sid", seq_id)])
             while query.next():
                 deal_sid, deal_qty, deal_price = readSQLrecord(query)
-                if reminder:
+                if reminder < 0:
                     next_deal_qty = reminder
                     reminder = 0
                 else:
@@ -340,7 +349,7 @@ class Ledger:
         buy_value = 0
         if self.getAmount(BookAccount.Assets, asset_id) > 0:
             query = executeSQL(self.db,
-                               "SELECT d.close_sid AS close, ABS(o.qty) - SUM(d.qty) AS remainder FROM deals AS d "
+                               "SELECT d.open_sid AS open, ABS(o.qty) - SUM(d.qty) AS remainder FROM deals AS d "
                                "LEFT JOIN sequence AS os ON os.type=3 AND os.id=d.open_sid "
                                "JOIN trades AS o ON o.id = os.operation_id "
                                "WHERE d.account_id=:account_id AND d.asset_id=:asset_id "
@@ -349,13 +358,22 @@ class Ledger:
                                [(":account_id", account_id), (":asset_id", asset_id)])
             if query.next(): # sid of Buy trade from last deal and non-matched reminder of last Sell trade
                 last_sid, reminder = readSQLrecord(query)
-                query = executeSQL(self.db,
-                                   "SELECT s.id, t.qty, t.price FROM trades AS t "
-                                   "LEFT JOIN sequence AS s ON s.type = 3 AND s.operation_id=t.id "
-                                   "WHERE t.qty > 0 AND t.asset_id = :asset_id AND t.account_id = :account_id "
-                                   "AND s.id < :sid AND s.id >= :last_sid",
-                                   [(":asset_id", asset_id), (":account_id", account_id),
-                                    (":sid", seq_id), (":last_sid", last_sid)])
+                if reminder > 0:   # Last Sell didn't fully match Buy Trade and we need to start from this partial Buy
+                    query = executeSQL(self.db,
+                                       "SELECT s.id, t.qty, t.price FROM trades AS t "
+                                       "LEFT JOIN sequence AS s ON s.type = 3 AND s.operation_id=t.id "
+                                       "WHERE t.qty > 0 AND t.asset_id = :asset_id AND t.account_id = :account_id "
+                                       "AND s.id < :sid AND s.id >= :last_sid",
+                                       [(":asset_id", asset_id), (":account_id", account_id),
+                                        (":sid", seq_id), (":last_sid", last_sid)])
+                else:   # Buy trade was fully matched by Sell in the last deal - we need to take next Buy Trade
+                    query = executeSQL(self.db,
+                                       "SELECT s.id, t.qty, t.price FROM trades AS t "
+                                       "LEFT JOIN sequence AS s ON s.type = 3 AND s.operation_id=t.id "
+                                       "WHERE t.qty > 0 AND t.asset_id = :asset_id AND t.account_id = :account_id "
+                                       "AND s.id < :sid AND s.id > :last_sid",
+                                       [(":asset_id", asset_id), (":account_id", account_id),
+                                        (":sid", seq_id), (":last_sid", last_sid)])
             else:  # There were no deals -> Select all purchases
                 reminder = 0
                 query = executeSQL(self.db,
@@ -364,7 +382,7 @@ class Ledger:
                                    "WHERE t.qty>0 AND t.asset_id=:asset_id AND t.account_id=:account_id AND s.id<:sid",
                                    [(":asset_id", asset_id), (":account_id", account_id), (":sid", seq_id)])
             while query.next():
-                deal_sid, deal_qty, deal_price = readSQLrecord(query)
+                deal_sid, deal_qty, deal_price = readSQLrecord(query)  # deal_sid -> trade_sid
                 if reminder > 0:
                     next_deal_qty = reminder
                     reminder = 0