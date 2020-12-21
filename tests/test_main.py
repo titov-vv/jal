993f383c96e55d2d228df08117b47545856e6530
titov-vv <55182132+titov-vv@users.noreply.github.com>
2020-12-21 20:17:12 +0300

    Test updated for new directory structure

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,7 +7,7 @@ from db.helpers import init_and_check_db
 def test_db_creation(tmp_path):
     # Prepare environment
     project_root = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
-    src_path = project_root + os.sep + Setup.INIT_SCRIPT_PATH
+    src_path = project_root + os.sep + 'jal' + os.sep + Setup.INIT_SCRIPT_PATH
     target_path = str(tmp_path) + os.sep + Setup.INIT_SCRIPT_PATH
     copyfile(src_path, target_path)
 
