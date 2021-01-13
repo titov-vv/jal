8201d79145ee66ab31aff5e7cf250fc1b063c904
titov-vv <55182132+titov-vv@users.noreply.github.com>
2021-01-13 10:51:50 +0300

    Test for backup load was added

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,12 +1,15 @@
 import os
 from shutil import copyfile
+import sqlite3
 
+from tests.fixtures import fill_deals, project_root
 from constants import Setup
-from jal.db.helpers import init_and_check_db
+from jal.db.helpers import init_and_check_db, get_dbfilename
+from jal.db.backup_restore import JalBackup
 
-def test_db_creation(tmp_path):
+
+def test_db_creation(tmp_path, project_root):
     # Prepare environment
-    project_root = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
     src_path = project_root + os.sep + 'jal' + os.sep + Setup.INIT_SCRIPT_PATH
     target_path = str(tmp_path) + os.sep + Setup.INIT_SCRIPT_PATH
     copyfile(src_path, target_path)
@@ -17,3 +20,25 @@ def test_db_creation(tmp_path):
     result_path = str(tmp_path) + os.sep + Setup.DB_PATH
     assert os.path.exists(result_path)
     assert os.path.getsize(result_path) > 0
+
+
+def test_backup_load(tmp_path, project_root):
+    test_db_creation(tmp_path, project_root)
+    project_root = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+
+    # Here backup is created without parent window - need to use with care
+    db_file_name = get_dbfilename(str(tmp_path) + os.sep)
+    backup = JalBackup(None, db_file_name)
+    backup.clean_db()
+    backup.backup_name = project_root + os.sep + "tests" + os.sep + "test_data" + os.sep + "deals_set.tgz"
+    backup.do_restore()
+
+    # Check
+    db = sqlite3.connect(db_file_name)
+    cursor = db.cursor()
+    cursor.execute("SELECT COUNT(*) FROM settings")
+    assert cursor.fetchone()[0] == 7
+
+
+def test_fifo(fill_deals):
+    pass
