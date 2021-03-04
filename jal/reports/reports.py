titov-vv/jal

commit 099e1edecbf4538be07777a2555fd8815a70fcd5

Draft: Simple model was added for tax estimation

jal/reports/reports.py
@@ -30,7 +30,7 @@ class PandasModel(QAbstractTableModel):
     def rowCount(self, parent=None):
         return self._data.shape[0]
 
-    def columnCount(self, parnet=None):
+    def columnCount(self, parent=None):
         return self._data.shape[1] + 1    # +1 as extra leftmost column serves as a category header
 
     def data(self, index, role=Qt.DisplayRole):
@@ -189,20 +189,20 @@ class Reports(QObject):
              (":begin", begin), (":end", end)])
         self.db.commit()
         self.query = executeSQL(self.db,
-                                "SELECT c.id, c.level, c.path, "
+                                "SELECT c.id AS id, c.level AS level, c.path AS category, "
                                 "strftime('%Y', datetime(p.row_key, 'unixepoch')) AS year, "
-                                "strftime('%m', datetime(p.row_key, 'unixepoch')) AS month, p.value "
+                                "strftime('%m', datetime(p.row_key, 'unixepoch')) AS month, p.value AS value"
                                 "FROM categories_tree AS c "
                                 "LEFT JOIN t_pivot AS p ON p.col_key=c.id "
                                 "ORDER BY c.path, year, month")
         table = []
         while self.query.next():
-            id, level, category, year, month, value = readSQLrecord(self.query)
-            turnover = value if value != '' else 0
+            record = readSQLrecord(self.query, named=True)
+            turnover = record['value'] if record['value'] != '' else 0
             table.append({
-                'category': category,
-                'Y': year,
-                'M': month,
+                'category': record['category'],
+                'Y': record['year'],
+                'M': record['month'],
                 'turnover': turnover
             })
         data = pd.DataFrame(table)

jal/db/tax_estimator.py
@@ -1,7 +1,30 @@
-from PySide2.QtCore import Qt
+import pandas as pd
+from PySide2.QtCore import Qt, QAbstractTableModel
 from PySide2.QtWidgets import QDialog, QTableView, QVBoxLayout, QFrame
-from jal.db.helpers import executeSQL, get_asset_name
+from jal.db.helpers import executeSQL, readSQLrecord, get_asset_name
 from jal.ui_custom.helpers import g_tr
+
+class TaxEstimatorModel(QAbstractTableModel):
+    def __init__(self, data):
+        QAbstractTableModel.__init__(self)
+        self._data = data
+
+    def rowCount(self, parent=None):
+        return self._data.shape[0]
+
+    def columnCount(self, parent=None):
+        return self._data.shape[1]
+
+    def data(self, index, role=Qt.DisplayRole):
+        if index.isValid():
+            if role == Qt.DisplayRole:
+                return self._data.iloc[index.row(), index.column()]
+        return None
+
+    def headerData(self, col, orientation, role=Qt.DisplayRole):
+        if (orientation == Qt.Horizontal and role == Qt.DisplayRole):
+            return str(self._data.columns[col])
+        return None
 
 class TaxEstimator(QDialog):
     def __init__(self, db, account_id, asset_id, asset_qty, parent=None):
@@ -10,6 +33,7 @@ class TaxEstimator(QDialog):
         self.account_id = account_id
         self.asset_id = asset_id
         self.asset_qty = asset_qty
+        self.dataframe = None
 
         self.setWindowTitle(g_tr('TaxEstimator', "Tax estimation for ") + get_asset_name(self.db, self.asset_id))
         self.setWindowFlag(Qt.Tool)
@@ -19,6 +43,8 @@ class TaxEstimator(QDialog):
         # Create widgets
         self.deals_view = QTableView(self)
         self.deals_view.setFrameShape(QFrame.Panel)
+        self.model = TaxEstimatorModel(self.dataframe)
+        self.deals_view.setModel(self.model)
         # Create layout and add widgets
         self.layout = QVBoxLayout()
         self.layout.setSpacing(0)
@@ -51,7 +77,8 @@ class TaxEstimator(QDialog):
                        [(":account_id", self.account_id), (":asset_id", self.asset_id)])
 
         query = executeSQL(self.db,
-                           "SELECT t.timestamp, t.qty, t.price AS o_price, oq.quote AS o_rate, "
+                           "SELECT strftime('%d/%m/%Y', datetime(t.timestamp, 'unixepoch')) AS timestamp, "
+                           "t.qty AS qty, t.price AS o_price, oq.quote AS o_rate, "
                            "lq.quote AS c_price, cq.quote AS c_rate FROM trades AS t "
                            "LEFT JOIN sequence AS s ON s.operation_id = t.id AND s.type = 3 "
                            "LEFT JOIN accounts AS ac ON ac.id = :account_id "
@@ -63,3 +90,10 @@ class TaxEstimator(QDialog):
                            "ORDER BY s.id DESC",
                            [(":account_id", self.account_id), (":asset_id", self.asset_id),
                             (":total_qty", self.asset_qty)])
+
+        table = []
+        while query.next():
+            record = readSQLrecord(query, named=True)
+            table.append(record)
+        data = pd.DataFrame(table)
+        self.dataframe = data

commit 7420b9a437b5c0809bbef8093a54e959cc3b61e3

Typo fixed that was introduced by 099e1ed

jal/reports/reports.py
@@ -187,7 +187,7 @@ class Reports(QObject):
              (":begin", begin), (":end", end)], commit=True)
         self.query = executeSQL("SELECT c.id AS id, c.level AS level, c.path AS category, "
                                 "strftime('%Y', datetime(p.row_key, 'unixepoch')) AS year, "
-                                "strftime('%m', datetime(p.row_key, 'unixepoch')) AS month, p.value AS value"
+                                "strftime('%m', datetime(p.row_key, 'unixepoch')) AS month, p.value AS value "
                                 "FROM categories_tree AS c "
                                 "LEFT JOIN t_pivot AS p ON p.col_key=c.id "
                                 "ORDER BY c.path, year, month")