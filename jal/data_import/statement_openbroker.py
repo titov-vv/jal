579eaa48953e1a421aab9321330d6b6d4d4a6edb
Vlad <55182132+titov-vv@users.noreply.github.com>
2021-08-01 17:36:05 +0300

    Open broker statement import: handling of asset type and exchange was modified.

--- jal/data_import/statement_openbroker.py
+++ jal/data_import/statement_openbroker.py
@@ -2,11 +2,43 @@ import logging
 from datetime import datetime
 
 from jal.widgets.helpers import g_tr
-from jal.data_import.statement import FOF, Statement_ImportError
+from jal.data_import.statement import FOF
 from jal.data_import.statement_xml import StatementXML
 from jal.net.helpers import GetAssetInfoFromMOEX
 
 
+# -----------------------------------------------------------------------------------------------------------------------
+class OpenBroker_AssetType:
+    NotSupported = -1
+    _asset_types = {
+        '': -1,
+        'Облигации': FOF.ASSET_BOND
+    }
+
+    def __init__(self, asset_type):
+        self.type = self.NotSupported
+        try:
+            self.type = self._asset_types[asset_type]
+        except KeyError:
+            logging.warning(g_tr('OpenBroker_AssetType', "Asset type isn't supported: ") + f"'{asset_type}'")
+
+
+# -----------------------------------------------------------------------------------------------------------------------
+class OpenBroker_Exchange:
+    _exchange_types = {
+        '': '',
+        "ПАО Московская биржа": "MOEX"
+    }
+
+    def __init__(self, exchange):
+        self.name = ''
+        try:
+            self.name = self._exchange_types[exchange]
+        except KeyError:
+            logging.warning(g_tr('OpenBroker_Exchange', "Exchange isn't supported: ") + f"'{exchange}'")
+
+
+# -----------------------------------------------------------------------------------------------------------------------
 class StatementOpenBroker(StatementXML):
     statements_path = '.'
     statement_tag = 'broker_report'
@@ -14,6 +46,11 @@ class StatementOpenBroker(StatementXML):
     def __init__(self):
         super().__init__()
         self.statement_name = g_tr('OpenBroker', "Open Broker statement")
+        open_loaders = {
+            OpenBroker_AssetType: self.attr_asset_type,
+            OpenBroker_Exchange: self.attr_exchange
+        }
+        self.attr_loader.update(open_loaders)
         self._sections = {
             StatementXML.STATEMENT_ROOT: {'tag': self.statement_tag,
                                           'level': '',
@@ -25,14 +62,28 @@ class StatementOpenBroker(StatementXML):
             'spot_portfolio_security_params': {'tag': 'item',
                                                'level': '',
                                                'values': [('ticker', 'symbol', str, None),
-                                                          ('security_type', 'type', str, None),
+                                                          ('security_type', 'type', OpenBroker_AssetType, OpenBroker_AssetType.NotSupported),
                                                           ('security_name', 'name', str, None),
                                                           ('isin', 'isin', str, ''),
                                                           ('security_grn_code', 'reg_code', str, ''),
-                                                          ('board_name', 'exchange', str, '')],
+                                                          ('board_name', 'exchange', OpenBroker_Exchange, '')],
                                                'loader': self.load_assets
                                                }
         }
+
+    # Convert attribute 'attr_name' value into json open-format asset type
+    @staticmethod
+    def attr_asset_type(xml_element, attr_name, default_value):
+        if attr_name not in xml_element.attrib:
+            return default_value
+        return OpenBroker_AssetType(xml_element.attrib[attr_name]).type
+
+    # Convert attribute 'attr_name' value into json open-format exchange name
+    @staticmethod
+    def attr_exchange(xml_element, attr_name, default_value):
+        if attr_name not in xml_element.attrib:
+            return default_value
+        return OpenBroker_Exchange(xml_element.attrib[attr_name]).name
 
     def load_header(self, header):
         self._data[FOF.PERIOD][0] = header['period_start']
@@ -42,36 +93,23 @@ class StatementOpenBroker(StatementXML):
                      f" - {datetime.utcfromtimestamp(header['period_end']).strftime('%Y-%m-%d')}")
 
     def load_assets(self, assets):
-        security_types = {
-            "Облигации": "bond"
-        }
-        exchanges = {
-            "ПАО Московская биржа": "MOEX"
-        }
         cnt = 0
         base = max([0] + [x['id'] for x in self._data[FOF.ASSETS]]) + 1
         for i, asset in enumerate(assets):
+            if asset['type'] == OpenBroker_AssetType.NotSupported:   # Skip not supported type of asset
+                continue
             asset['id'] = base + i
-            try:
-                asset['type'] = security_types[asset['type']]
-            except KeyError:
-                raise Statement_ImportError(
-                    g_tr('OpenBroker', "Security type not supported: ") + f"{asset['type']}")
-            if 'exchange' in asset:
-                try:
-                    asset['exchange'] = exchanges[asset['exchange']]
-                except KeyError:
-                    logging.warning(g_tr('OpenBroker', "Exchange not known: ") + asset['exchange'])
-                    del asset['exchange']
-                if asset['exchange'] == "MOEX":
-                    asset_info = GetAssetInfoFromMOEX(
-                        keys={"isin": asset['isin'], "regnumber": asset['reg_code'], "secid": asset['symbol']})
-                    if len(asset_info):
-                        asset_data = {'symbol': asset_info['symbol'], 'name': asset_info['name'],
-                                      'isin': asset_info['isin']}
-                        if asset_info['reg_code']:
-                            asset_data['reg_code'] = asset_info['reg_code']
-                        asset.update(asset_data)
+            if asset['exchange'] == "MOEX":
+                asset_info = GetAssetInfoFromMOEX(
+                    keys={"isin": asset['isin'], "regnumber": asset['reg_code'], "secid": asset['symbol']})
+                if len(asset_info):
+                    asset_data = {'symbol': asset_info['symbol'], 'name': asset_info['name'],
+                                  'isin': asset_info['isin']}
+                    if asset_info['reg_code']:
+                        asset_data['reg_code'] = asset_info['reg_code']
+                    asset.update(asset_data)
+            if asset['exchange'] == '':  # don't store empty exchange
+                asset.pop('exchange')
             cnt += 1
             self._data[FOF.ASSETS].append(asset)
         logging.info(g_tr('OpenBroker', "Securities loaded: ") + f"{cnt} ({len(assets)})")
