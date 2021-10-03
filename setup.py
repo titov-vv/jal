2528b2c035b6435d94b897d2954b68a3e8c1fb9d
Vlad <55182132+titov-vv@users.noreply.github.com>
2021-10-03 21:58:16 +0300

    PySide required version was fixed

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python"
     ],
-    install_requires=["lxml", "pandas", "PySide2>=5.15.2", "requests", "XlsxWriter"],
+    install_requires=["lxml", "pandas", "PySide6>=6.2.0", "requests", "XlsxWriter"],
     entry_points={
         'console_scripts': ['jal=jal.jal:main', ]
     },
