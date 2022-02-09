b2e2b2084ffb8f9473a640a4101b0e322be46136
Vlad <55182132+titov-vv@users.noreply.github.com>
2022-02-09 13:52:34 +0300

    Stock dividend was removed from CorporateActionWidget

--- jal/widgets/corporate_action_widget.py
+++ jal/widgets/corporate_action_widget.py
@@ -111,8 +111,7 @@ class CorporateActionWidget(AbstractOperationDetails):
                                              self.tr("Merger"),
                                              self.tr("Spin-Off"),
                                              self.tr("Symbol change"),
-                                             self.tr("Split"),
-                                             self.tr("Stock dividend")])
+                                             self.tr("Split")])
         self.type.setModel(self.combo_model)
 
         self.mapper.setItemDelegate(CorporateActionWidgetDelegate(self.mapper))
