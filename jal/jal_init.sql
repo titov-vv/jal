b391fb218d884c097055a9a244427b90599ce880
Vlad <55182132+titov-vv@users.noreply.github.com>
2022-02-15 17:20:49 +0300

    Operations order was adopted in operation_sequence view.

--- jal/jal_init.sql
+++ jal/jal_init.sql
@@ -546,25 +546,21 @@ CREATE VIEW operation_sequence AS
 SELECT m.op_type, m.id, m.timestamp, m.account_id, subtype
 FROM
 (
-    SELECT op_type, id, timestamp, account_id, NULL AS subtype FROM actions
+    SELECT op_type, 1 AS seq, id, timestamp, account_id, 0 AS subtype FROM actions
     UNION ALL
-    SELECT op_type, id, timestamp, account_id, type AS subtype FROM dividends
+    SELECT op_type, 2 AS seq, id, timestamp, account_id, type AS subtype FROM dividends
     UNION ALL
-    SELECT op_type, id, timestamp, account_id, type AS subtype FROM corp_actions
+    SELECT op_type, 3 AS seq, id, timestamp, account_id, type AS subtype FROM corp_actions
     UNION ALL
-    SELECT op_type, id, timestamp, account_id, NULL AS subtype FROM trades
+    SELECT op_type, 4 AS seq, id, timestamp, account_id, 0 AS subtype FROM trades
     UNION ALL
-    SELECT op_type, id, timestamp, account_id, subtype FROM
-    (
-        SELECT op_type, id, withdrawal_timestamp AS timestamp, withdrawal_account AS account_id, -1 AS subtype FROM transfers
-        UNION ALL
-        SELECT op_type, id, withdrawal_timestamp AS timestamp, fee_account AS account_id, 0 AS subtype FROM transfers WHERE NOT fee IS NULL
-        UNION ALL
-        SELECT op_type, id, deposit_timestamp AS timestamp, deposit_account AS account_id, 1 AS subtype FROM transfers
-        ORDER BY id
-    )
+    SELECT op_type, 5 AS seq, id, withdrawal_timestamp AS timestamp, withdrawal_account AS account_id, -1 AS subtype FROM transfers
+    UNION ALL
+    SELECT op_type, 5 AS seq, id, withdrawal_timestamp AS timestamp, fee_account AS account_id, 0 AS subtype FROM transfers WHERE NOT fee IS NULL
+    UNION ALL
+    SELECT op_type, 5 AS seq, id, deposit_timestamp AS timestamp, deposit_account AS account_id, 1 AS subtype FROM transfers
 ) AS m
-ORDER BY m.timestamp;
+ORDER BY m.timestamp, m.seq, m.subtype, m.id;
 
 
 -- View: all_transactions
