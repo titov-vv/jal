a8ab194b7bdd24ae6c438d2a82da2c5d5bbdda67
Vlad <55182132+titov-vv@users.noreply.github.com>
2022-03-16 14:49:03 +0300

    Force rebuild after db conversion.

--- jal/updates/jal_delta_32.sql
+++ jal/updates/jal_delta_32.sql
@@ -232,4 +232,5 @@ PRAGMA foreign_keys = 1;
 --------------------------------------------------------------------------------
 -- Set new DB schema version
 UPDATE settings SET value=32 WHERE name='SchemaVersion';
+INSERT OR REPLACE INTO settings(id, name, value) VALUES (7, 'RebuildDB', 1);
 COMMIT;
