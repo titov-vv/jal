b9869f235221961c28057097b28736a2f0e9cb9d
Vlad <55182132+titov-vv@users.noreply.github.com>
2022-04-27 19:24:13 +0300

    Just2Trade statement: crypto-currencies trades import

--- jal/data_import/broker_statements/just2trade.py
+++ jal/data_import/broker_statements/just2trade.py
@@ -77,6 +77,10 @@ class StatementJ2T(StatementXLS):
         logging.info(self.tr("Securities loaded: ") + f"{cnt}")
 
     def _load_deals(self):
+        self._load_stock_deals()
+        self._load_crypto_deals()
+
+    def _load_stock_deals(self):
         cnt = 0
         columns = {
             "number": "Номер сделки",
@@ -136,7 +140,61 @@ class StatementJ2T(StatementXLS):
             self._data[FOF.TRADES].append(trade)
             cnt += 1
             row += 1
-        logging.info(self.tr("Trades loaded: ") + f"{cnt}")
+        logging.info(self.tr("Stock trades loaded: ") + f"{cnt}")
+
+    def _load_crypto_deals(self):
+        cnt = 0
+        columns = {
+            "number": "Номер сделки",
+            "timestamp": "Дата сделки",
+            "settlement": "Дата расчетов",
+            "account_currency": "Валюта счета",
+            "asset_name": "Description",
+            "B/S": "Тип сделки",
+            "qty": "Кол-во",
+            "fee": "комиссия брокера в валюте счета",
+            "amount": "Итого в валюте счета",
+            "fee_ex": "Прочие комиссии в валюте счета"
+        }
+
+        row, headers = self.find_section_start("Сделки c виртуальными \(крипто\) инструментами", columns,
+                                               header_height=3)
+        if row < 0:
+            return
+        while row < self._statement.shape[0]:
+            if self._statement[self.HeaderCol][row] == '':
+                break
+            # Dates are stored as date in Excel report file for crypto deals
+            timestamp = int(self._statement[headers['timestamp']][row].replace(tzinfo=timezone.utc).timestamp())
+            settlement = int(self._statement[headers['settlement']][row].replace(tzinfo=timezone.utc).timestamp())
+            deal_number = str(self._statement[headers['number']][row])
+            asset_id = self.asset_id({'type': FOF.ASSET_CRYPTO, 'symbol': self._statement[headers['asset_name']][row],
+                                      'name': self._statement[headers['asset_name']][row],
+                                      'currency': self.currency_id('USD')})  # FIXME - replace hardcoded 'USD'
+            if self._statement[headers['B/S']][row].startswith('Купля'):
+                qty = self._statement[headers['qty']][row]
+            elif self._statement[headers['B/S']][row].startswith('Продажа'):
+                qty = -self._statement[headers['qty']][row]
+            else:
+                row += 1
+                logging.warning(self.tr("Unknown trade type: ") + self._statement[headers['B/S']][row])
+                continue
+            # Collect fees
+            fee = self._statement[headers['fee']][row] if self._statement[headers['fee']][row] else 0.0
+            if self._statement[headers['fee_ex']][row]:
+                fee += self._statement[headers['fee_ex']][row]
+            # Calculate price in account currency
+            amount = self._statement[headers['amount']][row]
+            price = -(amount + fee) / qty
+            assert price > 0.0
+            account_id = self._find_account_id(self._account_number, self._statement[headers['account_currency']][row])
+            new_id = max([0] + [x['id'] for x in self._data[FOF.TRADES]]) + 1
+            trade = {"id": new_id, "number": deal_number, "timestamp": timestamp, "settlement": settlement,
+                     "account": account_id, "asset": asset_id, "quantity": qty, "price": price, "fee": fee}
+            self._data[FOF.TRADES].append(trade)
+            cnt += 1
+            row += 1
+        logging.info(self.tr("Crypto trades loaded: ") + f"{cnt}")
 
     def _load_cash_transactions(self):
         pass
