d85a52dcb7c38daf9a23913edf3d9ff2ca45d864
Vlad <55182132+titov-vv@users.noreply.github.com>
2022-08-07 16:51:58 +0300

    JalSettings class was modified to work via common readSQL(), executeSQL() helper functions

--- jal/db/settings.py
+++ jal/db/settings.py
@@ -1,6 +1,4 @@
-import logging
-from PySide6.QtSql import QSqlQuery
-from jal.db.helpers import db_connection
+from jal.db.helpers import db_connection, readSQL, executeSQL
 
 
 class JalSettings:
@@ -8,26 +6,12 @@ class JalSettings:
         self.db = db_connection()
 
     def getValue(self, key, default=None):
-        get_query = QSqlQuery(self.db)
-        get_query.setForwardOnly(True)
-        get_query.prepare("SELECT value FROM settings WHERE name=:key")
-
-        value = default
-        get_query.bindValue(":key", key)
-        if not get_query.exec():
-            if not default:
-                logging.fatal(f"Failed to get settings for key='{key}'")
-            return value
-        if get_query.next():
-            value = get_query.value(0)
+        value = readSQL("SELECT value FROM settings WHERE name=:key", [(":key", key)])
+        if value is None:
+            value = default
         return value
 
     def setValue(self, key, value):
-        set_query = QSqlQuery(self.db)
-        set_query.prepare("INSERT OR REPLACE INTO settings(id, name, value) "
-                          "VALUES((SELECT id FROM settings WHERE name=:key), :key, :value)")
-        set_query.bindValue(":key", key)
-        set_query.bindValue(":value", value)
-        if not set_query.exec():
-            logging.fatal(f"Failed to set settings key='{key}' to value='{value}'")
-        self.db.commit()
+        executeSQL("INSERT OR REPLACE INTO settings(id, name, value) "
+                   "VALUES((SELECT id FROM settings WHERE name=:key), :key, :value)",
+                   [(":key", key), (":value", value)], commit=True)
