7fc4652bc6c1b726755774b3ad7e9beabf4300c8
Vlad <55182132+titov-vv@users.noreply.github.com>
2023-02-15 09:12:26 +0000

    Cache was added to JalCountry to improve GUI scrolling performance.

--- jal/db/country.py
+++ jal/db/country.py
@@ -2,17 +2,37 @@ from jal.db.db import JalDB
 
 
 class JalCountry(JalDB):
-    def __init__(self, id: int = 0, data: dict = None, search=False) -> None:
-        super().__init__()
-        self._id = id
+    db_cache = []
+
+    def __init__(self, country_id: int = 0, data: dict = None, search=False) -> None:
+        super().__init__(cached=True)
+        if not JalCountry.db_cache:
+            self._fetch_data()
+        self._id = country_id
         if self._valid_data(data):
             if search:
                 self._id = self._find_country(data)
-        self._data = self._read("SELECT name, code, iso_code FROM countries_ext WHERE id=:country_id",
-                                [(":country_id", self._id)], named=True)
+        try:
+            self._data = [x for x in self.db_cache if x['id'] == self._id][0]
+        except IndexError:
+            self._data = None
         self._name = self._data['name'] if self._data is not None else None
         self._code = self._data['code'] if self._data is not None else None
         self._iso_code = self._data['iso_code'] if self._data is not None else None
+
+    def invalidate_cache(self):
+        self._fetch_data()
+
+    # JalCountry maintains single cache available for all instances
+    @classmethod
+    def class_cache(cls) -> True:
+        return True
+
+    def _fetch_data(self):
+        JalCountry.db_cache = []
+        query = self._exec("SELECT * FROM countries_ext ORDER BY id")
+        while query.next():
+            JalCountry.db_cache.append(self._read_record(query, named=True))
 
     def id(self) -> int:
         return self._id
