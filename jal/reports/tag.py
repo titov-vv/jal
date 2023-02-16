8763819291b7c04096899dc8f2e730b85d6df1fb
Vlad <55182132+titov-vv@users.noreply.github.com>
2023-02-16 10:31:16 +0000

    TagReport now can handle incoming settings passed through show_report() call.

--- jal/reports/tag.py
+++ jal/reports/tag.py
@@ -47,6 +47,11 @@ class TagReportWindow(MdiWidget, Ui_TagReportWidget):
 
         self.connect_signals_and_slots()
 
+        if settings is not None:
+            self.ReportRange.setRange(settings['begin_ts'], settings['end_ts'])
+            self.ReportTagEdit.selected_id = settings['tag_id']
+            self.onTagChange()
+
     def connect_signals_and_slots(self):
         self.ReportRange.changed.connect(self.ReportTableView.model().setDateRange)
         self.ReportTagEdit.changed.connect(self.onTagChange)
