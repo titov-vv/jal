titov-vv/jal

commit 5aaebadc3c71834f597b0d593b6124c331ea3d67

OperationsModel.filterText() method was removed as it was not implemented. It is replaced with basic usage of QSortFilterProxyModel.

jal/db/operations_model.py
@@ -1,5 +1,5 @@
 from decimal import Decimal
-from PySide6.QtCore import Qt, Slot, QAbstractTableModel, QDate
+from PySide6.QtCore import Qt, Slot, QDate, QAbstractTableModel
 from PySide6.QtGui import QBrush, QFont
 from PySide6.QtWidgets import QStyledItemDelegate, QHeaderView
 from jal.constants import CustomColor
@@ -19,7 +19,6 @@ class OperationsModel(QAbstractTableModel):
         self._begin = 0
         self._end = 0
         self._account = 0
-        self._text_filter = ''
 
         self.prepareData()
 
@@ -124,14 +123,6 @@ class OperationsModel(QAbstractTableModel):
             self._end = end
         else:
             self._end = QDate.currentDate().endOfDay(Qt.UTC).toSecsSinceEpoch()
-        self.prepareData()
-
-    @Slot()
-    def filterText(self, filter):
-        if filter:    # FIXME Filter is not used with current query
-            self._text_filter = '' # f" AND (num_peer LIKE '%{filter}%' COLLATE NOCASE "
-        else:
-            self._text_filter = ''
         self.prepareData()
 
     def update(self):

jal/widgets/operations_widget.py
@@ -1,6 +1,6 @@
 from functools import partial
 
-from PySide6.QtCore import Qt, Slot, Signal, QDateTime
+from PySide6.QtCore import Qt, Slot, Signal, QDateTime, QSortFilterProxyModel
 from PySide6.QtGui import QAction
 from PySide6.QtWidgets import QMenu, QMessageBox
 from jal.ui.ui_operations_widget import Ui_OperationsWidget
@@ -44,7 +44,9 @@ class OperationsWidget(MdiWidget, Ui_OperationsWidget):
         self.balances_model.configureView()
 
         self.operations_model = OperationsModel(self.OperationsTableView)
-        self.OperationsTableView.setModel(self.operations_model)
+        self.operations_filtered_model = QSortFilterProxyModel(self.OperationsTableView)
+        self.operations_filtered_model.setSourceModel(self.operations_model)
+        self.OperationsTableView.setModel(self.operations_filtered_model)
         self.operations_model.configureView()
         self.OperationsTableView.setContextMenuPolicy(Qt.CustomContextMenu)
 
@@ -73,7 +75,7 @@ class OperationsWidget(MdiWidget, Ui_OperationsWidget):
         self.BalancesTableView.doubleClicked.connect(self.OnBalanceDoubleClick)
         self.ShowInactiveCheckBox.stateChanged.connect(self.BalancesTableView.model().toggleActive)
         self.DateRange.changed.connect(self.operations_model.setDateRange)
-        self.ChooseAccountBtn.changed.connect(self.OperationsTableView.model().setAccount)
+        self.ChooseAccountBtn.changed.connect(self.operations_model.setAccount)
         self.SearchString.editingFinished.connect(self.updateOperationsFilter)
         self.OperationsTableView.selectionModel().selectionChanged.connect(self.OnOperationChange)
         self.OperationsTableView.customContextMenuRequested.connect(self.onOperationContextMenu)
@@ -100,7 +102,8 @@ class OperationsWidget(MdiWidget, Ui_OperationsWidget):
 
     @Slot()
     def updateOperationsFilter(self):
-        self.OperationsTableView.model().filterText(self.SearchString.text())
+        self.OperationsTableView.model().setFilterFixedString(self.SearchString.text())
+        self.OperationsTableView.model().setFilterKeyColumn(-1)
 
     @Slot()
     def OnBalanceDoubleClick(self, index):
@@ -114,7 +117,7 @@ class OperationsWidget(MdiWidget, Ui_OperationsWidget):
             idx = selected.indexes()
             if idx:
                 selected_row = idx[0].row()
-                op_type, op_id = self.OperationsTableView.model().get_operation(selected_row)
+                op_type, op_id = self.operations_model.get_operation(selected_row)
         self.OperationsTabs.show_operation(op_type, op_id)
 
     @Slot()

commit aa85afada438f1ac9df2ca85717bfe97b736f6d9

Wrong index usage is fixed (the bug was introduced in 5aaebad with use of QSortFilterProxyModel and led to Segmentation fault) Operations search is enabled again (was disabled by workaround in 9c847c0)

jal/db/operations_model.py
@@ -1,5 +1,5 @@
 from decimal import Decimal
-from PySide6.QtCore import Qt, Slot, QDate, QAbstractTableModel
+from PySide6.QtCore import Qt, Slot, QDate, QAbstractTableModel, QModelIndex
 from PySide6.QtGui import QBrush, QFont
 from PySide6.QtWidgets import QStyledItemDelegate, QHeaderView
 from jal.constants import CustomColor
@@ -131,9 +131,13 @@ class OperationsModel(QAbstractTableModel):
     @Slot()
     def refresh(self):
         idx = self._view.selectionModel().selection().indexes()
+        idx = idx[0] if idx else QModelIndex()  # Take first selected or empty index
+        if self._view.model() != self:          # View uses some proxy model
+            idx = self._view.model().mapToSource(idx)
         self.prepareData()
-        if idx:
-            self._view.setCurrentIndex(idx[0])
+        if self._view.model() != self:
+            idx = self._view.model().mapFromSource(idx)
+        self._view.setCurrentIndex(idx)
 
     def prepareData(self):
         self._data = []

jal/widgets/operations_widget.py
@@ -1,6 +1,6 @@
 from functools import partial
 
-from PySide6.QtCore import Qt, Slot, Signal, QDateTime  #, QSortFilterProxyModel
+from PySide6.QtCore import Qt, Slot, Signal, QDateTime, QSortFilterProxyModel
 from PySide6.QtGui import QAction
 from PySide6.QtWidgets import QMenu, QMessageBox
 from jal.ui.ui_operations_widget import Ui_OperationsWidget
@@ -44,10 +44,9 @@ class OperationsWidget(MdiWidget, Ui_OperationsWidget):
         self.balances_model.configureView()
 
         self.operations_model = OperationsModel(self.OperationsTableView)
-        # It appears below change led to Segmentation fault after #5aaebadc
-        # self.operations_filtered_model = QSortFilterProxyModel(self.OperationsTableView)
-        # self.operations_filtered_model.setSourceModel(self.operations_model)
-        self.OperationsTableView.setModel(self.operations_model)
+        self.operations_filtered_model = QSortFilterProxyModel(self.OperationsTableView)
+        self.operations_filtered_model.setSourceModel(self.operations_model)
+        self.OperationsTableView.setModel(self.operations_filtered_model)
         self.operations_model.configureView()
         self.OperationsTableView.setContextMenuPolicy(Qt.CustomContextMenu)
 
@@ -103,9 +102,8 @@ class OperationsWidget(MdiWidget, Ui_OperationsWidget):
 
     @Slot()
     def updateOperationsFilter(self):
-        pass   # It appears below change led to Segmentation fault after #5aaebadc
-        # self.OperationsTableView.model().setFilterFixedString(self.SearchString.text())
-        # self.OperationsTableView.model().setFilterKeyColumn(-1)
+        self.OperationsTableView.model().setFilterFixedString(self.SearchString.text())
+        self.OperationsTableView.model().setFilterKeyColumn(-1)
 
     @Slot()
     def OnBalanceDoubleClick(self, index):