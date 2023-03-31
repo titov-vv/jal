e94e6768dbfae707a1a988e2644399c9ac02d783
Vlad <55182132+titov-vv@users.noreply.github.com>
2023-03-31 11:33:25 +0100

    Revert "Obsolete html-statement import files (for Quik and IBKR) were removed."
    This reverts commit 5c662bba34a5131100e14acf5781515593609727.

---
+++ jal/data_import/broker_statements/ibkr_html_old.py
@@ -0,0 +1,70 @@
+# This file is obsolete - it was created by special request to allow limited import from IBKR HTML reports
+# Import interface was changed so it was removed from menu but kept as a reference
+
+import logging
+import pandas
+from datetime import datetime, timezone
+from PySide6.QtWidgets import QApplication
+from jal.db.db import JalDB
+
+class IBKR_obsolete:
+    def __init__(self, filename):
+        self._filename = filename
+        self._account = ''
+
+    def tr(self, text):
+        return QApplication.translate("IBKR", text)
+
+    def load(self):
+        # Read first table with account details
+        try:
+            data = pandas.read_html(self._filename, match='Account Capabilities')
+        except:
+            logging.error(self.tr("Can't read statement file"))
+            return False
+        if len(data) != 1:
+            logging.error(self.tr("Can't get account details from the statement"))
+            return False
+        for i, row in data[0].iterrows():
+            if row[0] == 'Account':
+                self._account = row[1]
+        if self._account == '':
+            logging.error(self.tr("Can't get account number from the statement"))
+            return False
+
+        # Read Trades table
+        try:
+            data = pandas.read_html(self._filename, match='Date/Time', attrs = {'id': 'summaryDetailTable'})
+        except:
+            logging.error(self.tr("Can't read Trades table from statement file"))
+            return False
+        if len(data) != 1:
+            logging.error(self.tr("Can't get Trades table from the statement"))
+            return False
+        statement = data[0]
+        statement = statement[statement['Symbol'].notna()]
+
+        account_id = None
+        for i, row in statement.iterrows():
+            if row[0] == 'Forex':  # We reached end of Stock trades
+                break
+            if row[0].startswith('Total') or row[0] == 'Stocks' or row[0] == 'Symbol':  # Skip totals and headers
+                continue
+            if row[0] == row[1]:  # it's a currency header - match account
+                account_id = JalDB().get_account_id(self._account, row[0])
+                continue
+            if account_id is None:  # no reason to check further if we don't have valid account here
+                continue
+            asset_id = JalDB().get_asset_id(row[0])
+            if asset_id is None:
+                logging.warning(self.tr("Unknown asset ") + f"'{row[0]}'")
+                continue
+            timestamp = int(datetime.strptime(row[1], "%Y-%m-%d, %H:%M:%S").replace(tzinfo=timezone.utc).timestamp())
+            settlement = int(datetime.strptime(row[1][:10], "%Y-%m-%d").replace(tzinfo=timezone.utc).timestamp())
+            number = f"{i}"
+            qty = float(row[2])
+            price = float(row[3])
+            fee = float(row[6])
+            JalDB().add_trade(account_id, asset_id, timestamp, settlement, number, qty, price, -fee)
+        logging.info(self.tr("Load IBKR Activity statement completed"))
+        return True
---
+++ jal/data_import/broker_statements/quik_html_old.py
@@ -0,0 +1,109 @@
+# This file is obsolete - it was used for Quik HTML reports import for KIT and Uralsib brokers
+# But it was created before intermediate JSON statement validation was implememnted.
+# So it should be refactored to be a descendant of Statement class
+
+import logging
+import math
+import re
+import pandas
+from datetime import datetime, timezone
+from PySide6.QtWidgets import QApplication
+from jal.db.db import JalDB
+from jal.db.operations import Dividend
+
+
+# -----------------------------------------------------------------------------------------------------------------------
+# Strip white spaces from numbers imported form Quik html-report
+def convert_amount(val):
+    val = val.replace(' ', '')
+    try:
+        res = float(val)
+    except ValueError:
+        res = 0
+    return res
+
+
+# -----------------------------------------------------------------------------------------------------------------------
+class Quik:
+    ClientPattern = "^Код клиента: (.*)$"
+    DateTime = 'Дата и время заключения сделки'
+    TradeNumber = 'Номер сделки'
+    Symbol = 'Код инструмента'
+    Name = 'Краткое наименование инструмента'
+    Type = 'Направление'
+    Qty = 'Кол-во'
+    Price = 'Цена'
+    Amount = 'Объём'
+    Coupon = 'НКД'
+    SettleDate = 'Дата расчётов'
+    Buy = 'Купля'
+    Sell = 'Продажа'
+    Fee = 'Комиссия Брокера'
+    FeeEx = 'Суммарная комиссия ТС'    # This line is used in KIT Broker reports
+    FeeEx1 = 'Комиссия за ИТС'         # Below 3 lines are used in Uralsib Borker reports
+    FeeEx2 = 'Комиссия за организацию торговли'
+    FeeEx3 = 'Клиринговая комиссия'
+    Total = 'ИТОГО'
+
+    def __init__(self, filename):
+        self._filename = filename
+
+    def tr(self, text):
+        return QApplication.translate("Quik", text)
+
+    def load(self):
+        try:
+            data = pandas.read_html(self._filename, encoding='cp1251',
+                                    converters={self.Qty: convert_amount, self.Amount: convert_amount,
+                                                self.Price: convert_amount, self.Coupon: convert_amount})
+        except:
+            logging.error(self.tr("Can't read statement file"))
+            return False
+
+        report_info = data[0]
+        deals_info = data[1]
+        parts = re.match(self.ClientPattern, report_info[0][2])
+        if parts:
+            account_id = JalDB().get_account_id(parts.group(1))
+        else:
+            logging.error(self.tr("Can't get account number from the statement."))
+            return False
+        if account_id is None:
+            logging.error(self.tr("Account with number ") + f"{parts.group(1)}" +
+                          self.tr(" not found. Import cancelled."))
+            return False
+
+        for index, row in deals_info.iterrows():
+            if row[self.Type] == self.Buy:
+                qty = int(row[self.Qty])
+            elif row[self.Type] == self.Sell:
+                qty = -int(row[self.Qty])
+            elif row[self.Type][:len(self.Total)] == self.Total:
+                break  # End of statement reached
+            else:
+                logging.warning(self.tr("Unknown operation type ") + f"'{row[self.Type]}'")
+                continue
+            asset_id = JalDB().get_asset_id(row[self.Symbol])
+            if asset_id is None:
+                logging.warning(self.tr("Unknown asset ") + f"'{row[self.Symbol]}'")
+                continue
+            timestamp = int(
+                datetime.strptime(row[self.DateTime], "%d.%m.%Y %H:%M:%S").replace(tzinfo=timezone.utc).timestamp())
+            settlement = int(
+                datetime.strptime(row[self.SettleDate], "%d.%m.%Y").replace(tzinfo=timezone.utc).timestamp())
+            number = row[self.TradeNumber]
+            price = row[self.Price]
+            amount = row[self.Amount]
+            lot_size = math.pow(10, round(math.log10(amount / (price * abs(qty)))))
+            qty = qty * lot_size
+            fee = float(row[self.Fee])
+            if self.FeeEx in row:  # Broker dependent fee import
+                fee = fee + float(row[self.FeeEx])
+            else:
+                fee = fee + float(row[self.FeeEx1]) + float(row[self.FeeEx2]) + float(row[self.FeeEx3])
+            bond_interest = float(row[self.Coupon])
+            JalDB().add_trade(account_id, asset_id, timestamp, settlement, number, qty, price, fee)
+            if bond_interest != 0:
+                JalDB().add_dividend(Dividend.BondInterest, timestamp, account_id, asset_id,
+                                     bond_interest, "НКД", number)
+        return True
