bd3b0762332352052582dab5db995e2023144666
titov-vv <55182132+titov-vv@users.noreply.github.com>
2023-07-04 23:09:57 +0300

    Designer plugin for TableViewWithFooter was added.

---
+++ jal/widgets/designer_plugins/plugin_tableview_with_footer.py
@@ -0,0 +1,62 @@
+from custom.tableview_with_footer import TableViewWithFooter
+
+from PySide6.QtGui import QIcon
+from PySide6.QtDesigner import (QDesignerCustomWidgetInterface)
+
+
+DOM_XML = """
+<ui language='c++'>
+    <widget class='TableViewWithFooter' name='tableViewWithFooter'>
+        <property name='geometry'>
+            <rect>
+                <x>0</x>
+                <y>0</y>
+                <width>400</width>
+                <height>300</height>
+            </rect>
+        </property>
+    </widget>
+</ui>
+"""
+
+
+class TableViewWithFooterPlugin(QDesignerCustomWidgetInterface):
+    def __init__(self):
+        super().__init__()
+        self._initialized = False
+
+    def createWidget(self, parent):
+        t = TableViewWithFooter(parent)
+        return t
+
+    def domXml(self):
+        return DOM_XML
+
+    def group(self):
+        return ''
+
+    def icon(self):
+        return QIcon()
+
+    def includeFile(self):
+        return 'jal/widgets/custom/tableview_with_footer.h'
+
+    def initialize(self, form_editor):
+        if self._initialized:
+            return
+        self._initialized = True
+
+    def isContainer(self):
+        return False
+
+    def isInitialized(self):
+        return self._initialized
+
+    def name(self):
+        return 'TableViewWithFooter'
+
+    def toolTip(self):
+        return 'QTableView that has a footer'
+
+    def whatsThis(self):
+        return self.toolTip()
--- jal/widgets/register_designer_plugins.py
+++ jal/widgets/register_designer_plugins.py
@@ -3,6 +3,7 @@
 from designer_plugins.plugin_date_range_selector import DateRangeSelectorPlugin
 from designer_plugins.plugin_db_lookup_combobox import DbLookupComboBoxPlugin
 from designer_plugins.plugin_log_viewer import LogViewerPlugin
+from designer_plugins.plugin_tableview_with_footer import TableViewWithFooterPlugin
 
 from PySide6.QtDesigner import QPyDesignerCustomWidgetCollection
 
@@ -10,3 +11,4 @@ if __name__ == '__main__':
     QPyDesignerCustomWidgetCollection.addCustomWidget(DateRangeSelectorPlugin())
     QPyDesignerCustomWidgetCollection.addCustomWidget(DbLookupComboBoxPlugin())
     QPyDesignerCustomWidgetCollection.addCustomWidget(LogViewerPlugin())
+    QPyDesignerCustomWidgetCollection.addCustomWidget(TableViewWithFooterPlugin())
