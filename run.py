4cebef9d6e889624805b7eac1862a139dffcda5a
Vlad <55182132+titov-vv@users.noreply.github.com>
2023-12-27 14:36:14 +0000

    Faulthandler was enabled

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
-
+import faulthandler
 from jal.jal import main
 
 if __name__ == "__main__":
+    log_file_fd = open("jal_faults.log", "a")
+    faulthandler.enable(log_file_fd)
     main()
 
 # Below code is the same but initiates application via entry point defined in already installed package
