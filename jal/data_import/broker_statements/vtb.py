f12b95fb8648f47ba57005f692ad568274e5c4d3
Vlad <55182132+titov-vv@users.noreply.github.com>
2024-01-28 23:02:28 +0000

    Draft template for VTB statement import: assets loading was added

--- jal/data_import/broker_statements/vtb.py
+++ jal/data_import/broker_statements/vtb.py
@@ -1,6 +1,7 @@
 import logging
+import re
 from jal.data_import.statement_xls import StatementXLS
-from jal.data_import.statement import FOF
+from jal.data_import.statement import FOF, Statement_ImportError
 
 
 JAL_STATEMENT_CLASS = "StatementVTB"
@@ -14,6 +15,11 @@ class StatementVTB(StatementXLS):
     money_columns = {
         "name": "Валюта",
         "cash_end": "Плановый",
+    }
+    asset_section = "Отчет об остатках ценных бумаг"
+    asset_columns = {
+        "name": "Наименование ценной бумаги, \n№ гос. регистрации, ISIN",
+        "currency": "Валюта цены \n\(номинала для облигаций\)"
     }
 
     def __init__(self):
@@ -48,6 +54,36 @@ class StatementVTB(StatementXLS):
             account_id = self._find_account_id(self._account_number, currency)
             account = [x for x in self._data[FOF.ACCOUNTS] if x['id'] == account_id][0]
             account["cash_end"] = self.account_end_balance[currency]
+
+    def _load_assets(self):
+        AssetPattern = r"^(?P<name>.*), (?P<reg_number>.*), (?P<isin>.*)$"
+        cnt = 0
+        asset_type = ''
+        row, headers = self.find_section_start(self.asset_section, self.asset_columns)
+        if row < 0:
+            return False
+        while row < self._statement.shape[0]:
+            if self._statement[self.HeaderCol][row].startswith('ИТОГО') or self._statement[self.HeaderCol][row] == '':
+                break
+            asset_name = self._statement[headers['name']][row]
+            parts = re.match(AssetPattern, asset_name, re.IGNORECASE)
+            if parts is None:
+                asset_type = asset_name
+                row += 1
+                continue
+            asset_data = parts.groupdict()
+            if len(asset_data) != AssetPattern.count("(?P<"):  # check that expected number of groups was matched
+                raise Statement_ImportError(self.tr("Asset name miss some data ") + f"'{asset_name}'")
+            currency = self._statement[headers['currency']][row]
+            try:
+                currency_code = self.currency_substitutions[currency]
+            except KeyError:
+                currency_code = currency
+            _ = self.asset_id({'isin': asset_data['isin'], 'reg_number': asset_data['reg_number'],
+                               'currency': currency_code, 'search_offline': True, 'search_online': "MOEX"})
+            cnt += 1
+            row += 1
+        logging.info(self.tr("Securities loaded: ") + f"{cnt}")
 
     def _load_deals(self):
         pass
