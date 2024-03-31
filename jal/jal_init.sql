cab0740bcbe8e968c30f8e40b7aa876c9334c3af
Vlad <55182132+titov-vv@users.noreply.github.com>
2024-03-31 18:55:29 +0100

    Fix of forgotten db schema version change.

--- jal/jal_init.sql
+++ jal/jal_init.sql
@@ -690,7 +690,7 @@ END;
 
 
 -- Initialize default values for settings
-INSERT INTO settings(id, name, value) VALUES (0, 'SchemaVersion', 53);
+INSERT INTO settings(id, name, value) VALUES (0, 'SchemaVersion', 54);
 INSERT INTO settings(id, name, value) VALUES (1, 'TriggersEnabled', 1);
 -- INSERT INTO settings(id, name, value) VALUES (2, 'BaseCurrency', 1); -- Deprecated and ID shouldn't be re-used
 INSERT INTO settings(id, name, value) VALUES (3, 'Language', 1);
