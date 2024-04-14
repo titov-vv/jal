c8991791c95d9322346f3f4e4fc0c58b58a11314
Vlad <55182132+titov-vv@users.noreply.github.com>
2024-04-14 17:00:14 +0100

    Statement import: keep reg.number information in intermediate data.

--- jal/data_import/statement.py
+++ jal/data_import/statement.py
@@ -638,6 +638,10 @@ class Statement(QObject):   # derived from QObject to have proper string transla
                 symbol_id = max([0] + [x['id'] for x in self._data[FOF.SYMBOLS]]) + 1
                 symbol = {"id": symbol_id, "asset": -db_asset.id(), 'symbol': db_asset.symbol(asset_info['currency']), 'currency': asset_info['currency']}
                 self._data[FOF.SYMBOLS].append(symbol)
+                if 'reg_number' in asset_info:
+                    data_id = max([0] + [x['id'] for x in self._data[FOF.ASSETS_DATA]]) + 1
+                    reg_number = {"id": data_id, "asset": -db_asset.id(), "reg_number": asset_info['reg_number']}
+                    self._data[FOF.ASSETS_DATA].append(reg_number)
                 return asset['id']
         if asset is None and 'search_online' in asset_info:
             if asset_info['search_online'] == "MOEX":
