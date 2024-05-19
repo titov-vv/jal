3ebef0b6421761a57697c3fddfe5689e960df0ca
Vlad <55182132+titov-vv@users.noreply.github.com>
2024-05-19 16:22:30 +0100

    Removed old update sql-files.

--- jal/updates/jal_delta_39.sql
+++
@@ -1,8 +0,0 @@
-BEGIN TRANSACTION;
---------------------------------------------------------------------------------
-DROP VIEW IF EXISTS categories_tree;
-DROP VIEW IF EXISTS deals_ext;
---------------------------------------------------------------------------------
--- Set new DB schema version
-UPDATE settings SET value=39 WHERE name='SchemaVersion';
-COMMIT;
