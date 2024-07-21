99965ab3ce2817e2159422104091b4be1a3c5925
Vlad <55182132+titov-vv@users.noreply.github.com>
2024-07-21 17:21:02 +0100

    Foreign key for accounts.country_id was modified for keeping account if country was removed.

--- jal/updates/jal_delta_57.sql
+++ jal/updates/jal_delta_57.sql
@@ -58,16 +58,16 @@ CREATE TABLE temp_accounts AS SELECT * FROM accounts;
 DROP TABLE IF EXISTS accounts;
 CREATE TABLE accounts (
     id              INTEGER   PRIMARY KEY UNIQUE NOT NULL,
-    name            TEXT (64) NOT NULL UNIQUE,                                                                     -- human-readable name of the account
-    currency_id     INTEGER   REFERENCES assets (id) ON DELETE RESTRICT ON UPDATE CASCADE NOT NULL,                -- accounting currency for the account
-    active          INTEGER   DEFAULT (1) NOT NULL ON CONFLICT REPLACE,                                            -- 1 = account is active, 0 = inactive (hidden in UI)
-    investing       INTEGER   NOT NULL DEFAULT (0),                                                                -- 1 if account can hold investment assets, 0 otherwise
-    tag_id          INTEGER   REFERENCES tags (id) ON DELETE SET NULL ON UPDATE CASCADE,                           -- optional tag of the account
-    number          TEXT (32),                                                                                     -- human-readable number of account (as a reference to bank/broker documents)
-    reconciled_on   INTEGER   DEFAULT (0) NOT NULL ON CONFLICT REPLACE,                                            -- timestamp of last confirmed operation
-    organization_id INTEGER   REFERENCES agents (id) ON DELETE SET DEFAULT ON UPDATE CASCADE NOT NULL DEFAULT (1), -- Bank/Broker that handles account
-    country_id      INTEGER   REFERENCES countries (id) ON DELETE CASCADE ON UPDATE CASCADE DEFAULT (0) NOT NULL,  -- Location of the account
-    precision       INTEGER   NOT NULL DEFAULT (2)                                                                 -- number of digits after decimal points that is used by this account
+    name            TEXT (64) NOT NULL UNIQUE,                                                                       -- human-readable name of the account
+    currency_id     INTEGER   REFERENCES assets (id) ON DELETE RESTRICT ON UPDATE CASCADE NOT NULL,                  -- accounting currency for the account
+    active          INTEGER   DEFAULT (1) NOT NULL ON CONFLICT REPLACE,                                              -- 1 = account is active, 0 = inactive (hidden in UI)
+    investing       INTEGER   NOT NULL DEFAULT (0),                                                                  -- 1 if account can hold investment assets, 0 otherwise
+    tag_id          INTEGER   REFERENCES tags (id) ON DELETE SET NULL ON UPDATE CASCADE,                             -- optional tag of the account
+    number          TEXT (32),                                                                                       -- human-readable number of account (as a reference to bank/broker documents)
+    reconciled_on   INTEGER   DEFAULT (0) NOT NULL ON CONFLICT REPLACE,                                              -- timestamp of last confirmed operation
+    organization_id INTEGER   REFERENCES agents (id) ON DELETE SET DEFAULT ON UPDATE CASCADE NOT NULL DEFAULT (1),   -- Bank/Broker that handles account
+    country_id      INTEGER   REFERENCES countries (id) ON DELETE SET DEFAULT ON UPDATE CASCADE DEFAULT (0) NOT NULL,-- Location of the account
+    precision       INTEGER   NOT NULL DEFAULT (2)                                                                   -- number of digits after decimal points that is used by this account
 );
 INSERT INTO accounts (id, name, currency_id, active, investing, tag_id, number, reconciled_on, organization_id, country_id, precision)
   SELECT id, name, currency_id, active, investing, tag_id, number, reconciled_on, organization_id, country_id, precision FROM temp_accounts;
