c3492391668d2e355ea000877aa9f66ad9363148
Vlad <55182132+titov-vv@users.noreply.github.com>
2024-11-12 20:12:12 +0000

    Version 2024.11.1. DB schema version 59.

--- jal/constants.py
+++ jal/constants.py
@@ -9,7 +9,7 @@ class Setup:
     INI_FILE = "jal.ini"
     DB_PATH = "jal.sqlite"
     DB_CONNECTION = "JAL.DB"
-    DB_REQUIRED_VERSION = 58
+    DB_REQUIRED_VERSION = 59
     SQLITE_MIN_VERSION = "3.35"
     MAIN_WND_NAME = "JAL_MainWindow"
     INIT_SCRIPT_PATH = 'jal_init.sql'
