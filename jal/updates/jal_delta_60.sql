e79ac02b1aef5042a12377bfdd9aafeefa9c1cab
Vlad <55182132+titov-vv@users.noreply.github.com>
2025-12-29 13:34:45 +0000

    Fixed missing delimiters and order of statements in sql schema update file

--- jal/updates/jal_delta_60.sql
+++ jal/updates/jal_delta_60.sql
@@ -1,5 +1,9 @@
 BEGIN TRANSACTION;
 --------------------------------------------------------------------------------
+-- Remove views to prevent dependency errors
+DROP VIEW IF EXISTS currencies;
+DROP VIEW IF EXISTS assets_ext;
+--------------------------------------------------------------------------------
 CREATE TABLE IF NOT EXISTS asset_id (
     id INTEGER PRIMARY KEY UNIQUE NOT NULL,
     asset_id INTEGER REFERENCES assets (id) ON DELETE CASCADE ON UPDATE CASCADE NOT NULL,
@@ -8,16 +12,16 @@ CREATE TABLE IF NOT EXISTS asset_id (
 );
 -- Migrate existing ISINs to new asset_id table
 INSERT INTO asset_id (asset_id, id_type, id_value)
-  SELECT id, 2, isin FROM assets WHERE NOT (isin='' OR isin LIKE ' %')
+  SELECT id, 2, isin FROM assets WHERE NOT (isin='' OR isin LIKE ' %');
 -- Remove ISINs from asset_data table
-DELETE FROM asset_data WHERE id IN (SELECT d.id FROM asset_data d JOIN asset_id i ON d.asset_id=i.asset_id AND i.id_type=1 AND d.value=i.id_value)
+DELETE FROM asset_data WHERE id IN (SELECT d.id FROM asset_data d JOIN asset_id i ON d.asset_id=i.asset_id AND i.id_type=1 AND d.value=i.id_value);
 -- Migrate existing MOEX registration codes from asset_data to new asset_id table
 INSERT INTO asset_id (asset_id, id_type, id_value)
   SELECT asset_id, 5, value FROM asset_data d LEFT JOIN assets a ON d.asset_id=a.id
   WHERE datatype=1 AND value LIKE '1-__-%' AND NOT a.id IS NULL;
-DELETE FROM asset_data WHERE datatype=1 AND value LIKE '1-__-%'
+DELETE FROM asset_data WHERE datatype=1 AND value LIKE '1-__-%';
 -- Clean up any remaining registration data from asset_data table (There was a bit of mess)
-DELETE FROM asset_data WHERE datatype=1
+DELETE FROM asset_data WHERE datatype=1;
 --------------------------------------------------------------------------------
 -- Remove ISIN column from assets table
 ALTER TABLE assets DROP COLUMN isin;
@@ -26,14 +30,12 @@ ALTER TABLE asset_tickers DROP COLUMN description;
 ALTER TABLE asset_tickers RENAME TO asset_symbol;
 --------------------------------------------------------------------------------
 -- Update currencies view
-DROP VIEW IF EXISTS currencies;
 CREATE VIEW currencies AS
 SELECT a.id, s.symbol
     FROM assets AS a
     LEFT JOIN asset_symbol AS s ON s.asset_id = a.id AND  s.active = 1
     WHERE a.type_id = 1;
 -- Update assets_ext view and trigger
-DROP VIEW assets_ext;
 CREATE VIEW assets_ext AS
 SELECT a.id, a.type_id, t.symbol, a.full_name, i.id_value AS isin, t.currency_id, a.country_id, t.quote_source
     FROM assets a
@@ -41,7 +43,6 @@ SELECT a.id, a.type_id, t.symbol, a.full_name, i.id_value AS isin, t.currency_id
     LEFT JOIN asset_id i ON a.id = i.asset_id AND i.id_type = 1
     WHERE t.active = 1
     ORDER BY a.id;
-DROP TRIGGER on_asset_ext_delete;
 CREATE TRIGGER on_asset_ext_delete INSTEAD OF DELETE ON assets_ext FOR EACH ROW
 BEGIN
     DELETE FROM assets WHERE id = OLD.id;
